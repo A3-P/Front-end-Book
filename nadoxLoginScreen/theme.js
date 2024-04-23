7974a83b6f86700a17d80c0ffc58e863558ecb19
nadox00 <162769750+nadox00@users.noreply.github.com>
2024-04-23 19:03:11 -0300

    Delete nadoxLoginScreen directory
    Signed-off-by: nadox00 <162769750+nadox00@users.noreply.github.com>

--- nadoxLoginScreen/theme.js
+++
@@ -1,9 +0,0 @@
-const body = document.body;
-
-themeToggle.addEventListener("click", () => {
-    if (body.getAttribute("data-theme") === "light") {
-        body.setAttribute("data-theme", "dark");
-    } else {
-        body.setAttribute("data-theme", "light");
-    }
-});
