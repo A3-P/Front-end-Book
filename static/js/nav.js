00cc81c41db53e7d4d07a0a983d7e78e856feff8
Your Name <you@example.com>
2024-04-24 11:34:00 -0300

    filter add

--- static/js/nav.js
+++ static/js/nav.js
@@ -2,8 +2,10 @@ const profileBtn = document.getElementById("profileBtn");
 const profileMenuHeader = document.getElementById("profileMenuHeader").style;
 const goToLoginPage = document.getElementById("goToLoginPage");
 
+profileMenuHeader.display = "none";
+
 profileBtn.addEventListener("click", function(){
-    if(profileMenuHeader.display == "none"){
+    if(profileMenuHeader.display === "none"){
         profileMenuHeader.display = "flex";
     } else {
         profileMenuHeader.display = "none";
@@ -12,4 +14,19 @@ profileBtn.addEventListener("click", function(){
 
 goToLoginPage.addEventListener("click", function(){
     window.location.href = "nadoxLoginScreen/index.html";
+});
+
+
+
+const filterBtn = document.getElementById("filterBtn");
+const filter = document.getElementById("filter").style;
+
+filter.display = "none";
+
+filterBtn.addEventListener("click", function(){
+    if(filter.display === "none"){
+        filter.display = "flex";
+    } else {
+        filter.display = "none";
+    }
 });
