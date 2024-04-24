cb0d63a535fc835bedd5ba9727680ff216d69a43
Your Name <you@example.com>
2024-04-24 11:04:33 -0300

    header profile button

--- static/js/nav.js
+++ static/js/nav.js
@@ -1,4 +1,15 @@
 const profileBtn = document.getElementById("profileBtn");
+const profileMenuHeader = document.getElementById("profileMenuHeader").style;
+const goToLoginPage = document.getElementById("goToLoginPage");
+
 profileBtn.addEventListener("click", function(){
-    window.location.href ="nadoxLoginScreen/index.html";
+    if(profileMenuHeader.display == "none"){
+        profileMenuHeader.display = "flex";
+    } else {
+        profileMenuHeader.display = "none";
+    }
+});
+
+goToLoginPage.addEventListener("click", function(){
+    window.location.href = "nadoxLoginScreen/index.html";
 });
